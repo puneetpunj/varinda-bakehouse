31b3bcfbc0b6946a6b990eb0d300703ad4aa5618
Puneet Punj <puneetpunj88@gmail.com>
2020-01-20 21:00:00 +1100

    updates to add modal on cart icon

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,33 +4,45 @@ import defaultImg from "../images/product-1.jpeg";
 import PropTypes from "prop-types";
 import { memo } from "react";
 import { FaShoppingCart } from "react-icons/fa";
+import { ProductConsumer } from "../context";
 
 
 const Product = memo(({ product }) => {
-  const { name, slug, images, price } = product;
+  const { name, slug, images, price, id } = product;
   return (
-    <article className="product">
-      <div className="img-container">
-        <img src={images[0] || defaultImg} alt="single product" />
-        <div className="price-top">
-          <h6>${price}</h6>
-          <p>per packet</p>
-        </div>
-        <div className="cart-top">
-          <FaShoppingCart onClick={handleCartClick} />
-        </div>
-        <Link to={`/products/${slug}`} className="btn-primary product-link">
-          Ingredients
-        </Link>
-      </div>
-      <p className="product-info">{name}</p>
-    </article >
+    <ProductConsumer>
+      {value => {
+        return (
+          <article className="product">
+            <div className="img-container">
+              <Link to={`/products/${slug}`}>
+                <img src={images[0] || defaultImg} alt="single product" />
+              </Link>
+              <div className="price-top">
+                <h6>${price}</h6>
+                <p>per packet</p>
+              </div>
+              <div className="cart-top" onClick={() => {
+                value.addToCart(id);
+                value.openModal(id);
+              }} >
+                <FaShoppingCart />
+              </div>
+              {/* <Link to={`/products/${slug}`} className="btn-primary product-link">
+                Specification
+              </Link> */}
+            </div>
+            <Link to={`/products/${slug}`} style={{ "textDecoration": "none", "color": "black" }}>
+              <p className="product-info">{name}</p>
+            </Link>
+          </article >
+        )
+      }}
+
+    </ProductConsumer>
   );
 });
 
-const handleCartClick = () => {
-  console.log('hello')
-}
 Product.propTypes = {
   product: PropTypes.shape({
     name: PropTypes.string.isRequired,
